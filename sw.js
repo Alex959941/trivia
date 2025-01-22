5e5f439c3dd49c1ee1d08b54c23118223761c3bd
Alex959941 <1469436@sd61learn.ca>
2025-01-21 17:09:11 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,64 +1,33 @@
-// Define cache names
-const CACHE_NAME = 'my-app-cache-v1';
-const DYNAMIC_CACHE_NAME = 'my-app-dynamic-cache-v1';
+const version = 'v123';  // change this everytime you update the service worker
+                          // to force the browser to also update it.
 
-// Files to cache during install
-const STATIC_ASSETS = [
-  'manifest.json',
-  'index.html',
-  'style.css',
-  'myscript.js',
-  'icons/48.png',
-  'icons/72.png',
-  'icons/96.png',
-  'icons/144.png',
-  'icons/192.png',
-  'icons/512.png',
-  'icons/512_maskable.png',
-  'icons/512_rounded.png',
-  'sw.js'
-];
-
-// Install event: Cache static assets
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Opened cache');
-      return cache.addAll(STATIC_ASSETS);
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        '',
+        'index.html',
+        'style.css',
+        'myscript.js',
+        'icons/144.png',
+        'icons/192.png',
+        'icons/48.png',
+        'icons/512.png',
+        'icons/72.png',
+        'icons/96.png',
+        'icons/icon512_rounded.png',
+        'icons/icon512_maskable.png',
+        'manifest.json',
+        'sw.js'
+      ]);
     })
   );
 });
 
-// Activate event: Clean up old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME && cache !== DYNAMIC_CACHE_NAME) {
-            console.log('Deleting old cache:', cache);
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Fetch event: Network-first strategy
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    fetch(event.request)
-      .then(networkResponse => {
-        // If network fetch is successful, cache the response
-        return caches.open(DYNAMIC_CACHE_NAME).then(cache => {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-        });
-      })
-      .catch(() => {
-        // If network fetch fails, fallback to cache
-        return caches.match(event.request);
-      })
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
   );
 });
